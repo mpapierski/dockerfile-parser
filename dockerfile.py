4d72406d6161a6fb6997aef97795f43124e92841
Michał Papierski <michal@papierski.net>
2014-09-06 10:21:51 +0200

    Added some performance improvements.

--- dockerfile.py
+++ dockerfile.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def parse_raw_dockerfile(input_file):
   '''
   Parse Dockerfile and output list of "actual"
@@ -13,13 +16,13 @@ def parse_raw_dockerfile(input_file):
   ['RUN foo && bar', 'CMD /bin/true']
   '''
   result = []
-  current_line = ''
+  current_line = deque()
   for line in input_file:
     if line.startswith('#') or not line.strip():
       continue
-    current_line += line
+    current_line.append(line)
     if line.rstrip()[-1] != '\\':
-      result.append(current_line)
-      current_line = ''
+      result.append(''.join(current_line))
+      current_line.clear()
   return result
 
